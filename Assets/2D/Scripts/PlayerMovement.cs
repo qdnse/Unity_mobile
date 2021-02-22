bdeac9678f4d6cedb6e5e66ee5c711755da701b1
Dylan <dylan.ferreira@epitech.eu>
2021-02-22 23:55:44 +0100

    ADD Player Movement & Animation + DeathZone

---
+++ Assets/2D/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float RunSpeed = 40f;
+
+    float HorizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(HorizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            animator.SetBool("IsCrouching", true);
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            animator.SetBool("IsCrouching", false);
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(HorizontalMove * Time.fixedDeltaTime , crouch, jump);
+        jump = false;
+
+    }
+}
